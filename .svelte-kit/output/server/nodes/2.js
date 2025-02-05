2624bb6e051f56dbf8dc76f4b6b7bcb80b416369
NullRoz007 <spencerbog1@gmail.com>
2025-02-06 11:24:07 +1300

    cleanup

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.DRRPJKf5.js","_app/immutable/chunks/Bv2CX0pF.js","_app/immutable/chunks/BZzQXbv5.js","_app/immutable/chunks/0Eqm2C4h.js","_app/immutable/chunks/BvU8keI9.js","_app/immutable/chunks/oEOo3hgt.js","_app/immutable/chunks/DHHkUmq2.js","_app/immutable/chunks/CdaYFjyl.js"];
-export const stylesheets = ["_app/immutable/assets/state.c6i8ireL.css"];
-export const fonts = [];
--- build/_app/immutable/entry/start.C7CWq652.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/DcXORR_k.js";export{t as start};
